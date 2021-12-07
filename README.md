28a7a0501ec30aa9f52a1d73d6d8e4f625067adc
Charlie Highum <chighum41@gmail.com>
2021-12-06 18:16:07 -0700

    added list of options for license input

---
+++ README.md
@@ -0,0 +1,24 @@
+# title
+  ## Description
+    desc
+  ## Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributing](#contributing)
+    * [Testing](#tests)
+    * [Questions](#questions)
+  ## Installation
+    install
+  ## Usage
+    usage
+  ## License
+    MIT License
+  ## How to Contribute
+    contribute
+  ## Tests
+    test
+  ## Questions
+    If you have any questions, please feel free to contact me via Github or Email.
+    Github Profile: https:github.com/github
+    Email: email
--- index.js
+++ index.js
@@ -26,9 +26,51 @@ const questions = [
     name: "usage",
   },
   {
-    type: "input",
+    type: "list",
     message: "What license or badge does the project have?",
     name: "license",
+    choices: [
+      "BSD Zero Clause License",
+      "Academic Free License v3.0",
+      "GNU Affero General Public License v3.0",
+      "Apache License 2.0",
+      "Artistic License 2.0",
+      "BSD 2-Clause 'Simplified' License",
+      "BSD 3-Clause Clear License",
+      "BSD 3-Clause 'New' or 'Revised' License",
+      "BSD 4-Clause 'Original' or 'Old' License",
+      "Boost Software License 1.0",
+      "Creative Commons Attribution 4.0 International",
+      "Creative Commons Attribution Share Alike 4.0 International",
+      "Creative Commons Zero v1.0 Universal",
+      "CeCILL Free Software License Agreement v2.1",
+      "Educational Community License v2.0",
+      "Eclipse Public License 1.0",
+      "Eclipse Public License 2.0",
+      "European Union Public License 1.1",
+      "European Union Public License 1.2",
+      "GNU General Public License v2.0",
+      "GNU General Public License v3.0",
+      "ISC License",
+      "GNU Lesser General Public License v2.1",
+      "GNU Lesser General Public License v3.0",
+      "LaTeX Project Public License v1.3c",
+      "MIT No Attribution",
+      "MIT License",
+      "Mozilla Public License 2.0",
+      "Microsoft Public License",
+      "Microsoft Reciprocal License",
+      "Mulan Permissive Software License, Version 2",
+      "University of Illinois/NCSA Open Source License",
+      "Open Data Commons Open Database License v1.0",
+      "SIL Open Font License 1.1",
+      "Open Software License 3.0",
+      "PostgreSQL License",
+      "The Unlicense",
+      "Universal Permissive License v1.0",
+      "Vim License",
+      "zlib License",
+    ],
   },
   {
     type: "input",
@@ -68,7 +110,7 @@ function writeToFile(response) {
     * [License](#license)
     * [Contributing](#contributing)
     * [Testing](#tests)
-      * [Questions](#questions)
+    * [Questions](#questions)
   ## Installation
     ${response.install}
   ## Usage
@@ -80,6 +122,7 @@ function writeToFile(response) {
   ## Tests
     ${response.test}
   ## Questions
+    If you have any questions, please feel free to contact me via Github or Email.
     Github Profile: https:github.com/${response.github}
     Email: ${response.email}
 `;
